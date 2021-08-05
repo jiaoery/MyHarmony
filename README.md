3a6faca3f2c4a13ac796c7a3994cf3d2033e73b9
jiaoery <373105324@qq.com>
2021-08-05 16:20:25 +0800

    text

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MyHarmony
-init commit
+# MyHarmony：鸿蒙学习
+## 1.UI相关
+
+## 参考
+[鸿蒙官网：https://developer.harmonyos.com/cn/docs/documentation/doc-guides/document-outline-0000001064589184](https://developer.harmonyos.com/cn/docs/documentation/doc-guides/document-outline-0000001064589184)
--- entry/src/main/java/com/example/myharmony/slice/TextAbilitySlice.java
+++ entry/src/main/java/com/example/myharmony/slice/TextAbilitySlice.java
@@ -3,6 +3,8 @@ package com.example.myharmony.slice;
 import com.example.myharmony.ResourceTable;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Component;
+import ohos.agp.components.Text;
 
 public class TextAbilitySlice extends AbilitySlice {
 
@@ -10,5 +12,30 @@ public class TextAbilitySlice extends AbilitySlice {
     protected void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_text);
+        autoSize();
+        autoScroll();
+    }
+
+    private void autoScroll() {
+        Text text = (Text)findComponentById(ResourceTable.Id_text4);
+        // 跑马灯效果
+        text.setTruncationMode(Text.TruncationMode.AUTO_SCROLLING);
+        // 始终处于自动滚动状态
+        text.setAutoScrollingCount(Text.AUTO_SCROLLING_FOREVER);
+        // 启动跑马灯效果
+        text.startAutoScrolling();
+    }
+
+    private void autoSize() {
+        Text text = (Text) findComponentById(ResourceTable.Id_text3);
+        // 设置自动调整规则
+        text.setAutoFontSizeRule(30, 100, 1);
+        // 设置点击一次增多一个"T"
+        text.setClickedListener(new Component.ClickedListener() {
+            @Override
+            public void onClick(Component component) {
+                text.setText(text.getText() + "T");
+            }
+        });
     }
 }
